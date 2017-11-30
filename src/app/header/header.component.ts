1418fde92b7756070095eb02a960b8ea19cf4a79
Harald Konietzko <hk268766@fh-muenster.de>
2017-11-30 23:09:56 +0100

    Hab die Startseite etwas verändert.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor() {
+
+
+  }
+
 
   ngOnInit() {
   }
--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,13 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
+
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  angemeldet = false;
+
+  anmeldungen: object [] = [];
+
+  name: string;
+  passwort: object;
+
+  anmelden(name, passwort) {
+    this.name = name;
+    this.passwort = passwort;
+    this.anmeldungen.push(name);
+    this.anmeldungen.push(passwort);
+    this.angemeldet = true;
+  }
+
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
