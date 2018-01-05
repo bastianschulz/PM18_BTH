206aeb839c6fed3fc8ed1ee0b3ecda5131f66a6e
hkleckomio <hk268766@fh-muenster.de>
2018-01-05 14:50:13 +0100

    Blabla

--- src/app/service/backlog.service.ts
+++ src/app/service/backlog.service.ts
@@ -26,8 +26,8 @@ export class BacklogService {
 
 
   /* Adresse abhängig von Umgebung wählen */
-  private actionUrl: string = 'http://localhost:3000/api';
-  //private actionUrl: string = 'http://10.60.67.166:3000/api';
+  // private actionUrl: string = 'http://localhost:3000/api';
+  private actionUrl: string = 'http://10.60.67.166:3000/api';
   options: RequestOptions;
 
   constructor(private http: Http) {
